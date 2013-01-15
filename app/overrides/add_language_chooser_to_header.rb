d82a736384c3f4fcf5dcaa443adad3662a373e73
NTF <gary@newtechfusion.com>
2013-01-15 15:45:47 +0530

    initial commit

---
+++ app/overrides/add_language_chooser_to_header.rb
@@ -0,0 +1,23 @@
+# Deface::Override.new(:virtual_path => "spree/shared/_header",
+#                      :name => "header_language_chooser",
+#                      :insert_after => "#logo",
+#                      :disabled => false,
+#                      :text => "ddddddddddddd"
+# )
+Deface::Override.new(:virtual_path  => "spree/layouts/spree_application",
+                     :insert_before => "[data-hook='body']",
+                     :partial => "spree/shared/language_chooser",
+                     :name          => "english_locale"
+                     )
+# Deface::Override.new(:virtual_path => "spree/shared/_header",
+#                      :name => "header_language_chooser",
+#                      :insert_after => "[data-hook='body']",
+#                      #:partial => "spree/shared/language_chooser",
+#                      :text => "<code ",
+#                      :disabled => false)
+
+# Deface::Override.new(:virtual_path  => "spree/layouts/spree_application",
+#                      :insert_before => "[data-hook='body']",
+#                      :text => "<code erb-loud> params[:locale] </code>",
+#                      :name          => "params_locale"
+#                      )
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spreestore112::Application.config.secret_token = '38aea1548f63d815a80ae104648ebfb5060b8f81d5d7009d74db806ad608e3a9e548cff89c38479d117cbee35ca1def399fbbb17c192ab89bd42d30d992200c4'
